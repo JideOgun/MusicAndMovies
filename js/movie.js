fe20d66def6d7e221a690f3ed9e21ff3d43d74ef
CraigJensen <cmjensen82@gmail.com>
2021-12-14 20:47:53 -0600

    fetch function is working. Able to display movie posters

--- js/movie.js
+++ js/movie.js
@@ -3,12 +3,28 @@
 // Post movie genre list to HTML dropdown
 
 // Post movie list to HTML dropdown
-theMovieDb.common.api_key = '1fcb095f3dea632c59c58e8920d44217';
+var apiKey = '1fcb095f3dea632c59c58e8920d44217';
 
-function successCB(data) {
-  console.log('Success callback: ' + data);
-}
+// function successCB(data) {
+//   console.log('Success callback: ' + data);
+// }
 
-function errorCB(data) {
-  console.log('Error callback: ' + data);
-}
+// function errorCB(data) {
+//   console.log('Error callback: ' + data);
+// }
+
+var searchMovies = function () {
+  var apiUrl =
+    'https://api.themoviedb.org/3/movie/now_playing?api_key=' +
+    apiKey +
+    '&language=en-US&page=2';
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
+};
+searchMovies();
