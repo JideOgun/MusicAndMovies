0e44705718b4786d6b8193b68c56ee456e50b46e
CraigJensen <cmjensen82@gmail.com>
2021-12-14 18:26:04 -0600

    started functions

--- js/movie.js
+++ js/movie.js
@@ -3,3 +3,12 @@
 // Post movie genre list to HTML dropdown
 
 // Post movie list to HTML dropdown
+theMovieDb.common.api_key = '1fcb095f3dea632c59c58e8920d44217';
+
+function successCB(data) {
+  console.log('Success callback: ' + data);
+}
+
+function errorCB(data) {
+  console.log('Error callback: ' + data);
+}
