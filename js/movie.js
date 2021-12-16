84692699d81d35e7b7267194ba63593d72fe753f
CraigJensen <cmjensen82@gmail.com>
2021-12-16 17:05:13 -0600

    fetch for movie list setup

--- js/movie.js
+++ js/movie.js
@@ -6,29 +6,45 @@
 let apiKey = '1fcb095f3dea632c59c58e8920d44217';
 let baseUrl = 'https://api.themoviedb.org/3/';
 let genreSearch = 'genre/movie/list?api_key=';
+let moviesNowPlaying = 'movie/now_playing?api_key=';
+let langEn = '&language=en-US';
 
 let movieGenreSelect = document.querySelector('#moviegenre-select');
 let movieGenre = document.querySelector('#genre-option');
 
 var searchMoviesGenre = () => {
-  var apiUrl = ''.concat(baseUrl + genreSearch + apiKey + '&language=en-US');
+  let apiUrl = ''.concat(baseUrl + genreSearch + apiKey + langEn);
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        getMovie(data);
+        getMovieGenre(data);
       });
     }
   });
 };
 
-let getMovie = (data) => {
+let getMovieGenre = (data) => {
   for (let i = 0; i < data.genres.length; i++) {
     let movieGenre = document.createElement('option');
     movieGenre.textContent = data.genres[i].name;
 
     movieGenreSelect.appendChild(movieGenre);
   }
+  searchNowPlaying(data);
+};
+
+let searchNowPlaying = () => {
+  let apiUrl = ''.concat(baseUrl + moviesNowPlaying + apiKey + langEn);
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data.results[0].title);
+        console.log(apiUrl);
+      });
+    }
+  });
 };
 
 searchMoviesGenre();
