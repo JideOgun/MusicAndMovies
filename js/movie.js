feceff6db7946b246efd24a72c7c6e430052a3d9
CraigJensen <cmjensen82@gmail.com>
2021-12-16 20:57:24 -0600

    getting movies by genre

--- js/movie.js
+++ js/movie.js
@@ -6,12 +6,19 @@
 let apiKey = '1fcb095f3dea632c59c58e8920d44217';
 let baseUrl = 'https://api.themoviedb.org/3/';
 let genreSearch = 'genre/movie/list?api_key=';
-let moviesNowPlaying = 'movie/now_playing?api_key=';
+let moviesByGenre = 'discover/movie?api_key=' + apiKey + '&with_genres=';
 let langEn = '&language=en-US';
 
-let movieGenreSelect = document.querySelector('#moviegenre-select');
-let movieGenre = document.querySelector('#genre-option');
+let movieGenreNums = [
+  28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770,
+  53, 10752, 37,
+];
 
+console.log(moviesByGenre);
+// let movieGenreSelect = document.querySelector('#moviegenre-select');
+// let movieGenre = document.querySelector('#genre-option');
+
+// This will call TMDB api to generate genres
 var searchMoviesGenre = () => {
   let apiUrl = ''.concat(baseUrl + genreSearch + apiKey + langEn);
 
@@ -24,26 +31,31 @@ var searchMoviesGenre = () => {
   });
 };
 
+// This will append the genres to a drop down list
 let getMovieGenre = (data) => {
   for (let i = 0; i < data.genres.length; i++) {
     let movieGenre = document.createElement('option');
     movieGenre.textContent = data.genres[i].name;
 
-    movieGenreSelect.appendChild(movieGenre);
+    // movieGenreSelect.appendChild(movieGenre);
   }
   searchNowPlaying(data);
 };
 
 let searchNowPlaying = () => {
-  let apiUrl = ''.concat(baseUrl + moviesNowPlaying + apiKey + langEn);
+  for (let i = 0; i < movieGenreNums.length; i++) {
+    let apiUrl = ''.concat(
+      baseUrl + moviesByGenre + movieGenreNums[i] + langEn
+    );
 
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data.results[0].title);
-      });
-    }
-  });
+    fetch(apiUrl).then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          console.log(data);
+        });
+      }
+    });
+  }
 };
 
 searchMoviesGenre();
